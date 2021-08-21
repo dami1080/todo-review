47c50c7d3d505e6677ae5329ab15142844e51020
dami1080 <dareakintoye@gmail.com>
2021-08-20 15:49:17 -1100

    Add local storage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import taskComplete from './completed.js';
+import LocalStorageActions from './localStorageActions.js';
 
 const todoArr = [
   { description: 'Go to the cinema', completed: false, index: 1 },
